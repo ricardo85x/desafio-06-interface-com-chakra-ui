3fd4b146d4153d1ed67883618219490062dd9c13
Ricardo <ricardoinliner@yahoo.com>
2021-06-03 20:23:39 -0300

    gh-pages

---
+++ docs/_next/static/fY4K1frH8c6oF8aN0AVq5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-078d755cc2007f1eaaf9.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-75d970746228659a3abc.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
