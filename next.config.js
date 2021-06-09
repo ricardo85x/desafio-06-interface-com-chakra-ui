1551e4e88c2b5d6eb347494c2a339b2680f40a4f
Ricardo <ricardoinliner@yahoo.com>
2021-06-09 15:10:28 -0300

    gh-pages 01

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@ const withImages = require('next-images')
 const withSass = require('sass')
 
 const withPlugins = require('next-compose-plugins');
+const isDev = process.env.NODE_ENV !== "production";
+
 
 module.exports = withPlugins([
     withImages,
     withSass,
     {
         trailingSlash: true,
-    }
+        basePath: isDev ? "": "/desafio-06-interface-com-chakra-ui",
+        assetPrefix: isDev ? "": "/desafio-06-interface-com-chakra-ui/"    }
 ])
